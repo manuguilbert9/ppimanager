8c08474b3ea42b81dfa88f9a3c1df1d422669f65
Manu Guilbert <manu.guilbert9@gmail.com>
2025-09-28 08:36:51 +0000

    Mais les suggestions IA ne fonctionnent pas encore

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  })],
   model: 'googleai/gemini-1.5-pro-latest',
 });
